ea71c1d90dd91deaf6189fc96988b4ffa4148ec0
emailman <emailman@dtcc.edu>
2016-09-12 11:32:46 -0400

    Added images to simulate a coin flip

--- CIS209-Random Demo 1/Form1.Designer.cs
+++ CIS209-Random Demo 1/Form1.Designer.cs
@@ -30,16 +30,22 @@
         {
             this.btnRandInt = new System.Windows.Forms.Button();
             this.lblRandInt = new System.Windows.Forms.Label();
+            this.pbxTails = new System.Windows.Forms.PictureBox();
+            this.pbxHead = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTails)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHead)).BeginInit();
             this.SuspendLayout();
             // 
             // btnRandInt
             // 
+            this.btnRandInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRandInt.Location = new System.Drawing.Point(13, 13);
             this.btnRandInt.Name = "btnRandInt";
             this.btnRandInt.Size = new System.Drawing.Size(75, 23);
             this.btnRandInt.TabIndex = 0;
-            this.btnRandInt.Text = "Random Int";
+            this.btnRandInt.Text = "Flip";
             this.btnRandInt.UseVisualStyleBackColor = true;
+            this.btnRandInt.Click += new System.EventHandler(this.btnRandInt_Click);
             // 
             // lblRandInt
             // 
@@ -50,15 +56,42 @@
             this.lblRandInt.TabIndex = 1;
             this.lblRandInt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // pbxTails
+            // 
+            this.pbxTails.Image = global::CIS209_Random_Demo_1.Properties.Resources.Tails1;
+            this.pbxTails.Location = new System.Drawing.Point(13, 74);
+            this.pbxTails.Name = "pbxTails";
+            this.pbxTails.Size = new System.Drawing.Size(170, 170);
+            this.pbxTails.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxTails.TabIndex = 3;
+            this.pbxTails.TabStop = false;
+            this.pbxTails.Visible = false;
+            // 
+            // pbxHead
+            // 
+            this.pbxHead.Image = global::CIS209_Random_Demo_1.Properties.Resources.Heads1;
+            this.pbxHead.Location = new System.Drawing.Point(13, 74);
+            this.pbxHead.Name = "pbxHead";
+            this.pbxHead.Size = new System.Drawing.Size(170, 170);
+            this.pbxHead.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxHead.TabIndex = 2;
+            this.pbxHead.TabStop = false;
+            this.pbxHead.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(199, 260);
+            this.Controls.Add(this.pbxTails);
+            this.Controls.Add(this.pbxHead);
             this.Controls.Add(this.lblRandInt);
             this.Controls.Add(this.btnRandInt);
             this.Name = "Form1";
-            this.Text = "Random Things";
+            this.Text = "Coin Flip";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxTails)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxHead)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +100,8 @@
 
         private System.Windows.Forms.Button btnRandInt;
         private System.Windows.Forms.Label lblRandInt;
+        private System.Windows.Forms.PictureBox pbxHead;
+        private System.Windows.Forms.PictureBox pbxTails;
     }
 }
 
--- CIS209-Random Demo 1/Form1.cs
+++ CIS209-Random Demo 1/Form1.cs
@@ -1,20 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CIS209_Random_Demo_1
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRandInt_Click(object sender, EventArgs e)
+        {
+            int flip = rand.Next(2);
+            lblRandInt.Text = flip.ToString();
+
+            if (flip == 0)
+            {
+                pbxHead.Visible = true;
+                pbxTails.Visible = false;
+            }
+            else
+            {
+                pbxHead.Visible = false;
+                pbxTails.Visible = true;
+            }
         }
     }
 }
