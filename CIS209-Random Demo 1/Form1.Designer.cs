bdb11c9dda62fd49f62706c23b3c5cfdce9ea279
emailman <emailman@dtcc.edu>
2016-09-12 10:46:56 -0400

    Random Demo

--- CIS209-Random Demo 1/Form1.Designer.cs
+++ CIS209-Random Demo 1/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnRandInt = new System.Windows.Forms.Button();
+            this.lblRandInt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRandInt
+            // 
+            this.btnRandInt.Location = new System.Drawing.Point(13, 13);
+            this.btnRandInt.Name = "btnRandInt";
+            this.btnRandInt.Size = new System.Drawing.Size(75, 23);
+            this.btnRandInt.TabIndex = 0;
+            this.btnRandInt.Text = "Random Int";
+            this.btnRandInt.UseVisualStyleBackColor = true;
+            // 
+            // lblRandInt
+            // 
+            this.lblRandInt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRandInt.Location = new System.Drawing.Point(115, 13);
+            this.lblRandInt.Name = "lblRandInt";
+            this.lblRandInt.Size = new System.Drawing.Size(55, 23);
+            this.lblRandInt.TabIndex = 1;
+            this.lblRandInt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.lblRandInt);
+            this.Controls.Add(this.btnRandInt);
+            this.Name = "Form1";
+            this.Text = "Random Things";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRandInt;
+        private System.Windows.Forms.Label lblRandInt;
     }
 }
 
